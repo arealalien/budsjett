a3caef344a29b30b04db6c19953c70c04db9f441
Areal Alien ♡ <arild.edvin.andreassen@gmail.com>
2025-10-29 22:43:48 +0100

    bug fix

--- backend/src/lib/prisma.js
+++ backend/src/lib/prisma.js
@@ -1,12 +1,3 @@
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis;
-
-export const prisma =
-    globalForPrisma.prisma ??
-    new PrismaClient({
-        // helpful in dev; in prod you can keep only 'error'
-        log: process.env.NODE_ENV === 'production' ? ['error'] : ['query', 'error', 'warn'],
-    });
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+export const prisma = new PrismaClient();
