1d97c4a032a3d6328836b3706c4da247a1cf3e21
Areal Alien ♡ <arild.edvin.andreassen@gmail.com>
2025-10-31 00:28:34 +0100

    test

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ let cachedHandler;
 export default async function handler(req, res) {
     if (!cachedHandler) {
         const { default: serverless } = await import('serverless-http');
-        const { createApp } = await import('../backend/src/createApp.js');
+        const { createApp } = require('../backend/src/createApp.js'); // CJS require
         const app = createApp();
         cachedHandler = serverless(app);
     }
