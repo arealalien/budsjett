483d55b56dbabab8141c14aa86601ff2cab77435
Areal Alien ♡ <arild.edvin.andreassen@gmail.com>
2025-11-02 21:26:39 +0100

    test tiny

--- api/index.js
+++ api/index.js
@@ -1,20 +1,5 @@
-let appPromise;
-
-export default async function handler(req, res) {
-    try {
-        if (!appPromise) {
-            appPromise = (async () => {
-                const { createApp } = await import('../backend/src/createApp.js');
-                return createApp();
-            })();
-        }
-        const app = await appPromise;
-        return app(req, res);
-    } catch (err) {
-        console.error('API init error:', err);
-        res
-            .status(500)
-            .setHeader('content-type', 'text/plain')
-            .end('Init error:\n' + (err?.stack || String(err)));
-    }
+import { createApp } from '../backend/src/createApp.js';
+const app = createApp();
+export default function handler(req, res) {
+    return app(req, res);
 }
